b71cb5ce35eb6406521499b5d7fd00e938ea9fee
Rajendran k <rajendran_k@outlook.com>
2024-03-22 08:47:12 +0530

    Updated load balancer config

--- Challenge1/infrasolution/terraform/modules/app/main.tf
+++ Challenge1/infrasolution/terraform/modules/app/main.tf
@@ -2,28 +2,42 @@ provider "azurerm" {
   features {}
 }
 
+resource "azurerm_public_ip" "lb_ip_address" {
+  name                = "PublicIPForLB"
+  location            = var.location
+  resource_group_name = var.group_name
+  allocation_method   = "Static"
+}
+
 #creating load balancer
 resource "azurerm_lb" "app_lb" {
   name                = "my-app-lb"
   location            = var.location
   resource_group_name = var.group_name
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.lb_ip_address.id
+  }
 }
 
 #backend pool
-resource "azurerm_lb_backend_pool" "app_pool" {
+resource "azurerm_lb_backend_address_pool" "app_pool" {
   name                = "my-app-pool"
-  location            = var.location
-  resource_group_name = var.group_name
+  loadbalancer_id = azurerm_lb.app_lb.id
 }
 
 #connecting to vm to the backend address
-resource "azurerm_lb_backend_address" "app_backend" {
-  count           = 2
-  name            = format("app-vm-%d", count.index)
-  ip_address      = azurerm_network_interface.interface[count.index].primary_ip_address
-  frontend_port   = 80
-  backend_port    = 80
-  backend_pool_id = azurerm_lb_backend_pool.app_pool.id
+resource "azurerm_network_interface_backend_address_pool_association" "vm1" {
+  network_interface_id    = azurerm_network_interface.interface.id
+  ip_configuration_name   = "test config1"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.app_pool.id
+}
+
+#connecting to vm to the backend address
+resource "azurerm_network_interface_backend_address_pool_association" "vm2" {
+  network_interface_id    = azurerm_network_interface.interface.id
+  ip_configuration_name   = "test config2"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.app_pool.id
 }
 
 resource "azurerm_lb_rule" "app_rule" {
