c791eccf4d5dabd9aabea43fe8e44b2521937f8c
Rajendran k <rajendran_k@outlook.com>
2024-03-22 12:52:38 +0530

    Updated logic for return

--- Challenge3/coding.py
+++ Challenge3/coding.py
@@ -1,13 +1,14 @@
 def getvalue(obj, values):
     after_split = values.split('/')
     result = obj
+   
     try:
 
         for  i in after_split:
             if isinstance(result, dict) and i in result:
                 result = result[i]
             else:
-                result = None
+                
                 break
         return result
     except :
@@ -16,11 +17,12 @@ def getvalue(obj, values):
 
 data = {"a": {"b": {"c": "d"}}}
 
-print(getvalue(data,'a/b/c'))
+print(getvalue(data,'a/'))
 data = {"a": {"b": {"c": {"d"}}}}
-print(getvalue(data,'a/b/c'))
+'''print(getvalue(data,'a/b/c'))
 data = {"a": {"b": {"c": {"d"}}}}
 print(getvalue(data,'a/b/d'))
 obj2 = {"a": {"b": "c"}}
 key2 = "a/b/c"
 print(getvalue(obj2,key2))
+'''
